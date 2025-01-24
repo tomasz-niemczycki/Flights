e3170d97763d34e87334da8b808204ad890ed957
Tomasz Niemczycki <tomasz.niem@wp.pl>
2025-01-24 21:13:31 +0100

    testowano skrypt

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    $conn = mysqli_connect('localhost', 'root', '', 'spaceinc');
+    $queryAll = "SELECT * FROM flights";
+    $queryShip = "SELECT * FROM flights WHERE ship = 'FALCON 9 BLOCK 5'";
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +14,72 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1>Comming Flights:</h1>
+        <form action="index.php" method="get">
+            <label for="poll">Ship</label>
+            <input type="radio" name="poll" id="poll">
+            <button type="submit">Check</button>
+        </form>
+    <table>
+        <tr>
+            <th>Type</th>
+            <th>Payload</th>
+            <th>Date</th>
+            <th>Window</th>
+            <th>Ship</th>
+            <th>Propability</th>
+        </tr>
+<!--Script for showing results-->
+        <?php
+            if(isset($input)) { 
+                $resultShip = mysqli_query($conn, $queryShip);
+             while($row = mysqli_fetch_array($resultShip)) { 
+                echo "<tr>"; 
+                    echo "<td>"; 
+                        echo $row[1] . " "; 
+                    echo "</td>"; 
+                    echo "<td>"; 
+                        echo $row[2] . " "; 
+                    echo "</td>";
+                    echo "<td>"; 
+                        echo $row[3] . " "; 
+                    echo "</td>"; 
+                        echo "<td>"; 
+                    echo $row[4] . " "; 
+                        echo "</td>"; 
+                    echo "<td>"; 
+                        echo $row[5] . " "; 
+                    echo "</td>"; 
+                    echo "<td>"; 
+                        echo $row[6] . " "; 
+                    echo "</td>"; 
+                echo "</tr>"; };
+            }
+             else { 
+                $resultAll = mysqli_query($conn, $queryAll); 
+                while($row = mysqli_fetch_array($resultAll)) { 
+                    echo "<tr>"; 
+                        echo "<td>"; 
+                            echo $row[1] . " "; 
+                        echo "</td>"; 
+                        echo "<td>"; 
+                            echo $row[2] . " "; 
+                        echo "</td>";
+                        echo "<td>"; 
+                            echo $row[3] . " "; 
+                        echo "</td>"; 
+                            echo "<td>"; 
+                        echo $row[4] . " "; 
+                            echo "</td>"; 
+                        echo "<td>"; 
+                            echo $row[5] . " "; 
+                        echo "</td>"; 
+                        echo "<td>"; 
+                            echo $row[6] . " "; 
+                        echo "</td>"; 
+                    echo "</tr>"; };
+            }
+        ?>
+    </table>
 </body>
 </html>
